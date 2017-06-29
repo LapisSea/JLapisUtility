f74030d0b1dae9e3814f76f01103a41f9b3826d9
LapisSea <toni.tube.you@gmail.com>
2017-06-29 14:27:49 +0200

    lel

---
+++ src/com/lapissea/util/LogUtil.java
@@ -0,0 +1,291 @@
+package com.lapissea.util;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.function.IntConsumer;
+
+public class LogUtil{
+	
+	
+	public static final class __{
+		
+		protected static boolean		DEBUG_ACTIVE=true;
+		protected static boolean		DEBUG_INIT;
+		protected static boolean		EXTERNAL_INIT;
+		protected static IntConsumer	EXTERNAL_STREAM_OUT;
+		protected static IntConsumer	EXTERNAL_STREAM_ERR;
+		protected static Runnable		EXTERNAL_CLEAR;
+		public static PrintStream		OUT			=System.out;
+		
+		
+		
+		public static void INJECT_DEBUG_PRINT(boolean active){
+			if(DEBUG_INIT) return;
+			DEBUG_INIT=true;
+			DEBUG_ACTIVE=active;
+			if(!(DEBUG_ACTIVE=active)) return;
+			
+			System.setOut(new PrintStream(new DebugHeaderStream(System.out)));
+			System.setErr(new PrintStream(new DebugHeaderStream(System.err)));
+		}
+		
+		
+		private static final class DebugHeaderStream extends OutputStream{
+			
+			final OutputStream child;
+			
+			static boolean				LAST_CH_ENDL=true;
+			static DebugHeaderStream	LAST;
+			
+			//			static final Object LOCK=new Object();
+			
+			public DebugHeaderStream(OutputStream child){
+				this.child=child;
+			}
+			
+			@Override
+			public void write(int b) throws IOException{
+				//				synchronized(LOCK){
+				
+				if(LAST!=this){//prevent half of the line to be from other stream
+					if(!LAST_CH_ENDL) LAST.write('\n');//OI! END YO FUCKEN LINES
+					LAST=this;
+				}
+				
+				if(LAST_CH_ENDL){
+					LAST_CH_ENDL=false;
+					debugHeader(child);
+				}
+				
+				if(b=='\n') LAST_CH_ENDL=true;
+				
+				child.write((char)b);
+				//				}
+			}
+			
+			private static void debugHeader(OutputStream stream){
+				StackTraceElement[] trace=Thread.currentThread().getStackTrace();
+				int depth=12;
+				while(trace[depth].getClassName().equals(LogUtil.class.getName())){
+					depth++;
+				}
+				while(trace[depth].getLineNumber()==-1){
+					depth++;
+				}
+				
+				StackTraceElement stack=trace[depth];
+				String className=stack.getClassName();
+				try{
+					stream.write(("["+Thread.currentThread().getName()+"] ["+className.substring(className.lastIndexOf('.')+1)+":"+stack.getLineNumber()+"]: ").getBytes());
+				}catch(IOException e){
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		private static class SplitStream extends OutputStream{
+			
+			private final OutputStream s1,s2;
+			
+			public SplitStream(OutputStream s1, OutputStream s2){
+				this.s1=s1;
+				this.s2=s2;
+			}
+			
+			@Override
+			public void write(int b) throws IOException{
+				s1.write(b);
+				s2.write(b);
+			}
+		}
+		
+		public static void INJECT_FILE_LOG(String file){
+			try{
+				File logFile=new File(file);
+				FileOutputStream fileOut=new FileOutputStream(logFile);
+				
+				if(logFile.exists()) logFile.delete();
+				else{
+					logFile.getParentFile().mkdirs();
+					logFile.createNewFile();
+				}
+				System.setErr(new PrintStream(new SplitStream(System.err, fileOut)));
+				System.setOut(new PrintStream(new SplitStream(System.out, fileOut)));
+				
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * Note that this clears only external window.
+	 */
+	public static void clear(){
+		__.EXTERNAL_CLEAR.run();
+	}
+	
+	//================================================
+	public static void print(){
+		out("");
+	}
+	
+	public static void println(){
+		out("\n");
+	}
+	
+	public static void print(Object obj){
+		out(UtilL.toString(obj));
+	}
+	
+	public static void println(Object obj){
+		out(UtilL.toString(obj)+"\n");
+	}
+	
+	public static void print(Object...objs){
+		out(UtilL.toString(objs));
+	}
+	
+	public static void println(Object...objs){
+		out(UtilL.toString(objs)+"\n");
+	}
+	
+	//================================================
+	
+	public static void printlnEr(){
+		err("\n");
+	}
+	
+	public static void printEr(){
+		err("");
+	}
+	
+	public static void printEr(Object obj){
+		err(UtilL.toString(obj));
+	}
+	
+	public static void printlnEr(Object obj){
+		err(UtilL.toString(obj)+"\n");
+	}
+	
+	public static void printEr(Object...objs){
+		err(UtilL.toString(objs));
+	}
+	
+	public static void printlnEr(Object...objs){
+		err(UtilL.toString(objs)+"\n");
+	}
+	
+	//================================================
+	
+	public static void printFunctionTrace(int count, CharSequence splitter){
+		println(getFunctionTrace(count, splitter));
+	}
+	
+	public static String getFunctionTrace(int count, CharSequence splitter){
+		StringBuilder line=new StringBuilder();
+		
+		StackTraceElement[] trace=Thread.currentThread().getStackTrace();
+		if(count>=trace.length) count=trace.length-1;
+		//		for(StackTraceElement stack:trace){
+		//			stack.getMethodName()
+		//		}
+		for(int i=count+1;i>=2;i--){
+			line.append(trace[i].getMethodName()).append('(').append(trace[i].getLineNumber()).append(')');
+			if(i!=2) line.append(splitter);
+		}
+		return line.toString();
+	}
+	
+	/**
+	 * print fancy stuff and things
+	 */
+	public static void printWrapped(Object obj){
+		String[] data=UtilL.toString(obj).split("\n");
+		StringBuilder line=new StringBuilder();
+		
+		int length=0;
+		for(int i=0;i<data.length;i++){
+			String lin=(data[i]=data[i].replaceFirst("\\s+$", ""));
+			length=Math.max(length, lin.length());
+		}
+		
+		if(data.length>1){
+			length+=4;
+			for(int i=0;i<data.length;i++){
+				String lin=(data[i]="| "+data[i]+" |");
+				int diff=length-lin.length();
+				if(diff==0) continue;
+				
+				for(int j=0;j<diff;j++){
+					if(j%2==0) lin+="=";
+					else lin="="+lin;
+				}
+				
+				data[i]=lin;
+			}
+		}
+		
+		line.append("<<");
+		for(int i=0, j=length+4;i<j;i++){
+			line.append('=');
+		}
+		line.append(">>");
+		
+		String lineS=line.toString();
+		
+		out(lineS+"\n");
+		for(String lin:data)
+			out("<<=="+lin+"==>>\n");
+		out(lineS+"\n");
+	}
+	
+	public static <T> T printlnAndReturn(T obj){
+		println(obj);
+		return obj;
+	}
+	
+	public static void printStackTrace(String msg){
+		StringBuilder result=new StringBuilder();
+		
+		StackTraceElement[] a1=Thread.currentThread().getStackTrace();
+		
+		if(msg==null){
+			DateFormat dateFormat=new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+			Calendar cal=Calendar.getInstance();
+			result.append("Invoke time: ").append(dateFormat.format(cal.getTime())).append("\n");
+		}
+		else result.append(msg).append("\n");
+		
+		int length=0;
+		for(int i=2;i<a1.length;i++){
+			StackTraceElement a=a1[i];
+			String s=a.toString();
+			result.append(s).append("\n");
+			length=Math.max(s.length(), length);
+		}
+		for(int b=0;b<length/4;b++)
+			result.append("_/\\_");
+		
+		println(result);
+	}
+	
+	//================================================
+	
+	private synchronized static void out(String s){
+//		if(__.DEBUG_ACTIVE&&!__.DEBUG_INIT) System.err.println("LOG UTILITY DID NOT INJECT DEBUG HEADER!");
+		System.out.print(s);
+	}
+	
+	private synchronized static void err(String s){
+//		if(__.DEBUG_ACTIVE&&!__.DEBUG_INIT) System.err.println("LOG UTILITY DID NOT INJECT DEBUG HEADER!");
+		System.err.print(s);
+	}
+	
+}
---
+++ src/com/lapissea/util/UtilL.java
@@ -0,0 +1,359 @@
+package com.lapissea.util;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.nio.FloatBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+public class UtilL{
+	
+	private static final Map<Class<Object>,Function<Object,String>> CUSTOM_TO_STRINGS=new HashMap<>();
+	
+	public static final double	SQRT2D	=Math.sqrt(2);
+	public static final float	SQRT2F	=(float)SQRT2D;
+	
+	@SuppressWarnings("unchecked")
+	protected static <T> void __REGISTER_CUSTOM_TO_STRING(Class<T> type, Function<T,String> funct){
+		CUSTOM_TO_STRINGS.put((Class<Object>)type, (Function<Object,String>)funct);
+	}
+	
+	public static String toString(Object...objs){
+		StringBuilder print=new StringBuilder();
+		
+		if(objs!=null) for(int i=0;i<objs.length;i++){
+			Object a=objs[i];
+			if(isArray(a)) print.append(unknownArrayToString(a));
+			else if(a instanceof FloatBuffer) print.append(floatBufferToString((FloatBuffer)a));
+			else print.append(toString(a)+(i==objs.length-1?"":" "));
+		}
+		else print.append("null");
+		
+		return print.toString();
+	}
+	
+	public static String toString(Object obj){
+		
+		StringBuilder print=new StringBuilder();
+		
+		if(obj!=null){
+			if(isArray(obj)) print.append(unknownArrayToString(obj));
+			else if(obj instanceof FloatBuffer) print.append(floatBufferToString((FloatBuffer)obj));
+			else{
+				Class<?> type=obj.getClass();
+				Function<Object,String> fun=CUSTOM_TO_STRINGS.get(type);
+				if(fun!=null) print.append(fun.apply(obj));
+				else{
+					Entry<Class<Object>,Function<Object,String>> ent=CUSTOM_TO_STRINGS.entrySet().stream().filter(e->instanceOf(e.getKey(), type)).findFirst().orElse(null);
+					if(ent!=null) print.append(ent.getValue().apply(obj));
+					else print.append(obj.toString());
+				}
+			}
+		}
+		else print.append("null");
+		
+		return print.toString();
+	}
+	
+	public static boolean isArray(Object object){
+		return object!=null&&object.getClass().isArray();
+	}
+	
+	public static String floatBufferToString(FloatBuffer buff){
+		StringBuilder print=new StringBuilder("Buffer{");
+		
+		buff=buff.duplicate();
+		buff.limit(buff.capacity());
+		if(buff.capacity()>0){
+			int j=0;
+			print.append(buff.get(j));
+			for(j=1;j<buff.capacity();j++)
+				print.append(", ").append(buff.get(j));
+		}
+		print.append('}');
+		return print.toString();
+	}
+	
+	private static String unknownArrayToString(Object arr){
+		if(arr instanceof boolean[]) return Arrays.toString((boolean[])arr);
+		if(arr instanceof float[]) return Arrays.toString((float[])arr);
+		if(arr instanceof byte[]) return Arrays.toString((byte[])arr);
+		if(arr instanceof int[]) return Arrays.toString((int[])arr);
+		if(arr instanceof long[]) return Arrays.toString((long[])arr);
+		if(arr instanceof short[]) return Arrays.toString((short[])arr);
+		if(arr instanceof char[]) return Arrays.toString((char[])arr);
+		if(arr instanceof double[]) return Arrays.toString((double[])arr);
+		if(arr instanceof Object[]) return Arrays.toString((Object[])arr);
+		return "ERR: "+arr;
+	}
+	
+	public static boolean TRUE(){
+		return true;
+	}
+	
+	public static void sleep(long millis){
+		try{
+			Thread.sleep(millis);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static void sleep(long millis, int nanos){
+		try{
+			Thread.sleep(millis, nanos);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static <T> Stream<T> stream(Enumeration<T> e){
+		return StreamSupport.stream(
+				new Spliterators.AbstractSpliterator<T>(Long.MAX_VALUE, Spliterator.ORDERED){
+					
+					@Override
+					public boolean tryAdvance(Consumer<? super T> action){
+						if(e.hasMoreElements()){
+							action.accept(e.nextElement());
+							return true;
+						}
+						return false;
+					}
+					
+					@Override
+					public void forEachRemaining(Consumer<? super T> action){
+						while(e.hasMoreElements()){
+							action.accept(e.nextElement());
+						}
+					}
+				}, false);
+	}
+	
+	public static <T> T[] array(List<T> list){
+		if(list.isEmpty()) return null;
+		
+		@SuppressWarnings("unchecked")
+		T[] a=(T[])UtilL.array(list.get(0).getClass(), list.size());
+		return list.toArray(a);
+	}
+	
+	public static <K,V> void doAndClear(Map<K,V> collection, BiConsumer<K,V> toDo){
+		if(collection.isEmpty()) return;
+		collection.forEach(toDo);
+		collection.clear();
+	}
+	
+	public static <T> void doAndClear(Collection<T> collection, Consumer<T> toDo){
+		if(collection.isEmpty()) return;
+		collection.stream().forEach(toDo);
+		collection.clear();
+	}
+	
+	public static void startDaemonThread(Runnable run, String name){
+		Thread t=new Thread(run, name);
+		t.setDaemon(true);
+		t.start();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <T> T[] array(Class<T> componentType, int length){
+		return (T[])Array.newInstance(componentType, length);
+	}
+	
+	public static boolean instanceOf(Class<?> left, Class<?> right){
+		if(left==right) return true;
+		try{
+			left.asSubclass(right);
+			return true;
+		}catch(Exception ignored){}
+		return false;
+	}
+	
+	public static boolean instanceOf(Object left, Class<?> right){
+		return left!=null&&instanceOf(left.getClass(), right);
+	}
+	
+	public static boolean instanceOf(Class<?> left, Object right){
+		return instanceOf(left, right.getClass());
+	}
+	
+	public static boolean instanceOf(Object left, Object right){
+		return instanceOf(left.getClass(), right.getClass());
+	}
+	
+	public static Serializable fromString(String s){
+		//		byte[] data=Base64.getDecoder().decode(s);
+		byte[] data=s.getBytes();
+		Object o=null;
+		try{
+			ObjectInputStream ois=new ObjectInputStream(new ByteArrayInputStream(data));
+			try{
+				o=ois.readObject();
+			}catch(ClassNotFoundException e){
+				e.printStackTrace();
+			}
+			ois.close();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		return (Serializable)o;
+	}
+	
+	public static String toString(Serializable o) throws IOException{
+		ByteArrayOutputStream baos=new ByteArrayOutputStream();
+		ObjectOutputStream oos=new ObjectOutputStream(baos);
+		oos.writeObject(o);
+		oos.close();
+		//		return Base64.getEncoder().encodeToString(baos.toByteArray());
+		return new String(baos.toByteArray());
+	}
+	
+	public static void closeSilenty(Closeable closeable){
+		try{
+			closeable.close();
+		}catch(IOException e){}
+	}
+	
+	public static String stringFill(int length, char c){
+		char[] ch=new char[length];
+		Arrays.fill(ch, c);
+		return new String(ch);
+	}
+	
+	public static List<Field> getAllFields(Class<?> type){
+		return getAllFields(new ArrayList<Field>(), type);
+	}
+	
+	public static List<Field> getAllFields(List<Field> fields, Class<?> type){
+		fields.addAll(Arrays.asList(type.getDeclaredFields()));
+		
+		if(type.getSuperclass()!=null){
+			getAllFields(fields, type.getSuperclass());
+		}
+		
+		return fields;
+	}
+	
+	public static String compress(String data){
+		try{
+			return new String(compress(data.getBytes("UTF-8")));
+		}catch(Exception e){
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public static byte[] compress(byte[] data){
+		try{
+			if((data==null)||(data.length==0)) return null;
+			ByteArrayOutputStream obj=new ByteArrayOutputStream();
+			GZIPOutputStream gzip=new GZIPOutputStream(obj);
+			gzip.write(data);
+			gzip.close();
+			return obj.toByteArray();
+		}catch(Exception e){
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public static String decompress(final byte[] compressed){
+		try{
+			String outStr="";
+			if(compressed==null||compressed.length==0) return "";
+			if(isCompressed(compressed)){
+				GZIPInputStream gis=new GZIPInputStream(new ByteArrayInputStream(compressed));
+				BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(gis, "UTF-8"));
+				
+				String line;
+				while((line=bufferedReader.readLine())!=null){
+					outStr+=line;
+				}
+			}
+			else{
+				outStr=new String(compressed);
+			}
+			return outStr;
+		}catch(Exception e){
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public static boolean isCompressed(final byte[] compressed){
+		return (compressed[0]==(byte)(GZIPInputStream.GZIP_MAGIC))&&(compressed[1]==(byte)(GZIPInputStream.GZIP_MAGIC>>8));
+	}
+	
+	public static Class<?> findClosestCommonSuper(Class<?> a, Class<?> b){
+		while(!a.isAssignableFrom(b))
+			a=a.getSuperclass();
+		return a;
+	}
+	
+	public static void iterate(Object data, Consumer<Object> consumer){
+		iterate(data, Object.class, consumer);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <T> void iterate(Object data, Class<T> type, Consumer<T> consumer){
+		if(data==null) return;
+		
+		if(instanceOf(data, type)) consumer.accept((T)data);
+		else if(data instanceof Iterable){
+			((Iterable<?>)data).forEach(mat->consumer.accept((T)mat));
+		}
+		else if(data.getClass().isArray()){
+			Class<?> typeTx=data.getClass().getComponentType();
+			
+			if(instanceOf(typeTx, type)){
+				for(T tx:(T[])data){
+					consumer.accept(tx);
+				}
+			}
+		}
+	}
+	
+	public static byte[] readAll(InputStream is){
+		ByteArrayOutputStream buffer;
+		try{
+			buffer=new ByteArrayOutputStream();
+			
+			int nRead;
+			byte[] data=new byte[16384];
+			
+			while((nRead=is.read(data, 0, data.length))!=-1){
+				buffer.write(data, 0, nRead);
+			}
+		}catch(Exception e){
+			throw new RuntimeException(e);
+		}
+		return buffer.toByteArray();
+	}
+	
+	public static boolean emptyOrNull(String string){
+		return string==null||string.isEmpty();
+	}
+	
+}
