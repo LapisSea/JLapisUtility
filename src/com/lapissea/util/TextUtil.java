ddbab711cac54be0951ed50ca4fd6d2c7f1f5e85
LapisSea <toni.tube.you@gmail.com>
2017-10-28 13:16:06 +0200

    nio buffers

--- src/com/lapissea/util/TextUtil.java
+++ src/com/lapissea/util/TextUtil.java
@@ -1,6 +1,8 @@
 package com.lapissea.util;
 
+import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 import java.util.*;
 import java.util.function.Function;
 
@@ -9,9 +11,40 @@ import static com.lapissea.util.UtilL.*;
 public class TextUtil{
 	
 	private static final Map<Class<Object>, Function<Object, String>> CUSTOM_TO_STRINGS=new HashMap<>();
+	
 	@SuppressWarnings("unchecked")
 	public static <T> void __REGISTER_CUSTOM_TO_STRING(Class<T> type, Function<T, String> funct){
 		CUSTOM_TO_STRINGS.put((Class<Object>)type, (Function<Object, String>)funct);
+	}
+	
+	static{
+		__REGISTER_CUSTOM_TO_STRING(FloatBuffer.class, buffer->{
+			StringBuilder print=new StringBuilder("FloatBuffer[");
+			for(int i=0;i<buffer.capacity();){
+				print.append(buffer.get(i));
+				if(++i<buffer.capacity()) print.append(", ");
+			}
+			print.append(']');
+			return print.toString();
+		});
+		__REGISTER_CUSTOM_TO_STRING(IntBuffer.class, buffer->{
+			StringBuilder print=new StringBuilder("IntBuffer[");
+			for(int i=0;i<buffer.capacity();){
+				print.append(buffer.get(i));
+				if(++i<buffer.capacity()) print.append(", ");
+			}
+			print.append(']');
+			return print.toString();
+		});
+		__REGISTER_CUSTOM_TO_STRING(ByteBuffer.class, buffer->{
+			StringBuilder print=new StringBuilder("ByteBuffer[");
+			for(int i=0;i<buffer.capacity();){
+				print.append(buffer.get(i));
+				if(++i<buffer.capacity()) print.append(", ");
+			}
+			print.append(']');
+			return print.toString();
+		});
 	}
 	
 	public static String toStringArray(Object[] arr){
@@ -21,29 +54,11 @@ public class TextUtil{
 		for(int i=0;i<arr.length;i++){
 			Object a=arr[i];
 			if(isArray(a)) print.append(unknownArrayToString(a));
-			else if(a instanceof FloatBuffer) print.append(floatBufferToString((FloatBuffer)a));
 			else print.append(toString(a));
 			if(i!=arr.length-1) print.append(", ");
 		}
 		
 		return print.append("]").toString();
-	}
-	
-	public static String floatBufferToString(FloatBuffer buff){
-		if(buff==null) return "null";
-		
-		StringBuilder print=new StringBuilder("Buffer{");
-		
-		buff=buff.duplicate();
-		buff.limit(buff.capacity());
-		if(buff.capacity()>0){
-			int j=0;
-			print.append(buff.get(j));
-			for(j=1;j<buff.capacity();j++)
-				print.append(", ").append(buff.get(j));
-		}
-		print.append('}');
-		return print.toString();
 	}
 	
 	private static String unknownArrayToString(Object arr){
@@ -67,7 +82,6 @@ public class TextUtil{
 		for(int i=0;i<objs.length;i++){
 			Object a=objs[i];
 			if(isArray(a)) print.append(unknownArrayToString(a));
-			else if(a instanceof FloatBuffer) print.append(floatBufferToString((FloatBuffer)a));
 			else print.append(toString(a));
 			if(i!=objs.length-1) print.append(" ");
 		}
@@ -81,13 +95,15 @@ public class TextUtil{
 		StringBuilder print=new StringBuilder();
 		
 		if(isArray(obj)) print.append(unknownArrayToString(obj));
-		else if(obj instanceof FloatBuffer) print.append(floatBufferToString((FloatBuffer)obj));
 		else{
 			Class                    type=obj.getClass();
 			Function<Object, String> fun =CUSTOM_TO_STRINGS.get(type);
 			if(fun!=null) print.append(fun.apply(obj));
 			else{
-				Map.Entry<Class<Object>, Function<Object, String>> ent=CUSTOM_TO_STRINGS.entrySet().stream().filter(e->instanceOf(e.getKey(), type)).findFirst().orElse(null);
+				Map.Entry<Class<Object>, Function<Object, String>> ent=
+					CUSTOM_TO_STRINGS.entrySet().stream().filter(e->instanceOf(type, e.getKey()))
+					                 .findFirst()
+					                 .orElse(null);
 				if(ent!=null) print.append(ent.getValue().apply(obj));
 				else print.append(obj.toString());
 			}
@@ -95,6 +111,7 @@ public class TextUtil{
 		
 		return print.toString();
 	}
+	
 	public static String plural(String word, int count){
 		if(count==1) return word;
 		switch(word.charAt(word.length()-1)){
@@ -124,70 +141,96 @@ public class TextUtil{
 	}
 	
 	public static String wrappedString(String... lines){
-		StringBuilder line=new StringBuilder();
+		if(lines==null||lines.length==0) return "";
 		
-		int length=0;
-		for(int i=0;i<lines.length;i++){
-			String lin=lines[i]=lines[i].replaceFirst("\\s+$", "");
-			length=Math.max(length, lin.length());
+		StringBuilder result =new StringBuilder();
+		StringBuilder padding=new StringBuilder();
+		
+		int width=Arrays.stream(lines).mapToInt(String::length).max().orElse(0)+2;
+		result.append('/');
+		for(int i=0;i<width;i++)result.append('-');
+		result.append("\\\n");
+		
+		for(String line : lines){
+			int diff=width-line.length();
+			int l   =diff/2;
+			int r   =diff-l;
+			
+			result.append('|');
+			
+			if(padding.length()>l) padding.setLength(l);
+			else while(padding.length()<l) padding.append(' ');
+			result.append(padding);
+			
+			result.append(line);
+			
+			if(padding.length()>r) padding.setLength(l);
+			else while(padding.length()<r) padding.append(' ');
+			result.append(padding);
+			
+			result.append("|\n");
 		}
 		
-		if(lines.length>1){
-			length+=4;
-			for(int i=0;i<lines.length;i++){
-				String lin =lines[i]="| "+lines[i]+" |";
-				int    diff=length-lin.length();
-				if(diff==0) continue;
-				
-				for(int j=0;j<diff;j++){
-					if(j%2==0) lin+="=";
-					else lin="="+lin;
-				}
-				
-				lines[i]=lin;
-			}
-		}
+		result.append('\\');
+		for(int i=0;i<width;i++)result.append('-');
+		result.append('/');
 		
-		line.append("<<");
-		for(int i=0, j=length+4;i<j;i++){
-			line.append('=');
-		}
-		line.append(">>");
-		
-		String lineS=line.toString();
-		
-		StringBuilder res=new StringBuilder(lineS+"\n");
-		for(String lin : lines) res.append("<<==").append(lin).append("==>>\n");
-		res.append(lineS);
-		return res.toString();
+		return result.toString();
 	}
 	
 	public static List<String> wrapLongString(String str, int width){
 		List<String>  result=new ArrayList<>(2);
 		StringBuilder line  =new StringBuilder();
 		
-		int x=0;
 		
 		for(int i=0;i<str.length();i++){
 			char c=str.charAt(i);
-			if(Character.isWhitespace(c)){
-				if(x>=width){
-					x=0;
-					result.add(line.toString());
-					line.setLength(0);
-					while(i+1<str.length()&&Character.isWhitespace(c=str.charAt(i+1))&&c!='\n') i++;
-					continue;
+			
+			if(line.length()>=width){
+				
+				if(Character.isWhitespace(c)){
+					
+					int lastSpace=line.length()-1;
+					while(lastSpace>0&&Character.isWhitespace(line.charAt(lastSpace-1))) lastSpace--;
+					while(lastSpace>0&&!Character.isWhitespace(line.charAt(lastSpace-1))) lastSpace--;
+					
+					if(lastSpace!=0){
+						if(line.length()-lastSpace>width) return wrapLongString(str, line.length()-lastSpace);
+						
+						String lastWord=line.substring(lastSpace);
+						line.setLength(lastSpace);
+						result.add(line.toString());
+						line.setLength(0);
+						line.append(lastWord);
+					}
+
+//					result.add(line.toString());
+//					line.setLength(0);
+//					while(i+1<str.length()&&Character.isWhitespace(c=str.charAt(i+1))&&c!='\n') i++;
+//					continue;
 				}
 			}
 			line.append(c);
 			if(c=='\n'){
-				x=0;
 				result.add(line.toString());
 				line.setLength(0);
-			}else x++;
+			}
 		}
-		if(x!=0) result.add(line.toString());
+		result.add(line.toString());
 		
 		return result;
+	}
+	
+	public static String join(Collection<? extends CharSequence> strings, String s){
+		StringBuilder result=new StringBuilder();
+		
+		Iterator<? extends CharSequence> i=strings.iterator();
+		
+		while(i.hasNext()){
+			result.append(i.next());
+			if(i.hasNext()) result.append(s);
+		}
+		
+		return result.toString();
 	}
 }
