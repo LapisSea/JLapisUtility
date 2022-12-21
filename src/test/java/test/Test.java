c226f5fb7cb9a1edefe32c7764c3d2b444dd864d
LapisSea <toni.tube.you@gmail.com>
2022-12-21 03:44:40 +0100

    Add test

---
+++ src/test/java/test/Test.java
@@ -0,0 +1,13 @@
+package test;
+
+import com.lapissea.util.LogUtil;
+
+public class Test{
+	
+	public static void main(String[] args){
+		LogUtil.Init.attach(0);
+		LogUtil.println("hi. wierd letters: ˘°€|~^˘°˛`˙´đđšćž¨|\uD83D\uDE0A");
+		
+	}
+	
+}
